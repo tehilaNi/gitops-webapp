9b41094b70fde0bd1c3d42ef092ed313076fd20a
tehilaNi <tehilani@walla.co.il>
2026-02-11 20:40:54 +0200

    class task

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify
+import os
+
 app = Flask(__name__)
+
 @app.route("/api/hello")
 def hello():
-    return jsonify(message="Hello from Kubernetes via Argo CD!")
+    message = os.getenv("APP_MESSAGE", "This is the Default Message")
+    return jsonify(message=message)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
